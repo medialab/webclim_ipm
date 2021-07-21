39d065e92be98afd4bd2426528b36757b9612e74
heloisethero <thero.heloise@gmail.com>
2021-07-21 18:28:29 +0200

    add the June srop timeseries figure

---
+++ code/create_section_1_figures.py
@@ -0,0 +1,51 @@
+import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from utils import import_data, save_figure, plot_engagement_timeserie
+
+
+def import_crowdtangle_group_data():
+
+    df_list = []
+    for file_index in range(5):
+        df_list.append(import_data(file_name="posts_fake_news_2021_group_" + str(file_index + 1) + ".csv"))
+    posts_group_df = pd.concat(df_list)
+
+    print("\nThere are {} 'repeat offender' Facebook groups.".format(posts_group_df.account_id.nunique()))
+
+    posts_page_df = import_data(file_name="posts_fake_news_2021_page.csv")
+    print("There are {} 'repeat offender' Facebook pages.".format(posts_page_df.account_id.nunique()))
+
+    posts_df = pd.concat([posts_group_df, posts_page_df])
+
+    posts_df['date'] = pd.to_datetime(posts_df['date'])
+    posts_df['engagement'] = posts_df[["share", "comment", "reaction"]].sum(axis=1)
+
+    return posts_df
+
+
+def plot_repeat_average_timeseries(posts_df):
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.subplot()
+
+    plt.title('')
+
+    plot_engagement_timeserie(ax, posts_df)
+
+    plt.ylim(0, 60)
+    plt.axvline(x=np.datetime64('2020-06-09'), color='C3', linestyle='--')
+
+    plt.tight_layout()
+    save_figure('repeat_average_timeseries')
+
+
+if __name__=="__main__":
+
+    posts_df = import_crowdtangle_group_data()
+    # create_repeat_example_timeseries_figure(posts_df)
+
+    plot_repeat_average_timeseries(posts_df)
