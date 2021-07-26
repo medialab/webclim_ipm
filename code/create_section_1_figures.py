73d63d92595863dac648531e742b7e1a6f70d7ef
heloisethero <thero.heloise@gmail.com>
2021-07-26 12:08:53 +0200

    add repeat example figure

--- code/create_section_1_figures.py
+++ code/create_section_1_figures.py
@@ -1,6 +1,5 @@
-import datetime
-
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
@@ -26,6 +25,114 @@ def import_crowdtangle_group_data():
     posts_df['engagement'] = posts_df[["share", "comment", "reaction"]].sum(axis=1)
 
     return posts_df
+
+
+def clean_crowdtangle_url_data(post_url_df):
+
+    post_url_df = post_url_df[post_url_df["platform"] == "Facebook"]
+    post_url_df = post_url_df.dropna(subset=['account_id', 'url'])
+
+    post_url_df = post_url_df.sort_values(by=['datetime'], ascending=True)
+    post_url_df = post_url_df.drop_duplicates(subset=['account_id', 'url'], keep='first')
+    post_url_df['account_id'] = post_url_df['account_id'].astype(int)
+
+    post_url_df = post_url_df[['url', 'account_id', 'account_name', 'account_subscriber_count', 'date']]
+
+    return post_url_df
+
+
+def compute_fake_news_dates(post_url_df, url_df, account_id):
+
+    post_url_group_df = post_url_df[post_url_df["account_id"]==account_id]
+    fake_news_dates = []
+
+    for url in post_url_group_df["url"].unique().tolist():
+        potential_dates = []
+
+        # We consider the date of the Facebook post or posts:
+        potential_dates.append(post_url_group_df[post_url_group_df["url"] == url]["date"].values[0])
+        # We consider the date of the fact-check:
+        potential_dates.append(url_df[url_df['url']==url]["Date of publication"].values[0])
+
+        potential_dates = [np.datetime64(date) for date in potential_dates]
+        date_to_plot = np.max(potential_dates)
+        fake_news_dates.append(date_to_plot)
+        
+    fake_news_dates.sort()
+
+    return fake_news_dates
+
+
+def compute_repeat_offender_periods(fake_news_dates):
+
+    repeat_offender_periods = []
+
+    if len(fake_news_dates) > 1:
+        for index in range(1, len(fake_news_dates)):
+            if fake_news_dates[index] - fake_news_dates[index - 1] < np.timedelta64(90, 'D'):
+
+                repeat_offender_periods.append([
+                    fake_news_dates[index],
+                    fake_news_dates[index - 1] + np.timedelta64(90, 'D')
+                ])
+
+    return repeat_offender_periods
+
+
+def merge_overlapping_periods(overlapping_periods):
+    
+    if len(overlapping_periods) == 0:
+        return []
+    
+    else:
+        overlapping_periods.sort(key=lambda interval: interval[0])
+        merged_periods = [overlapping_periods[0]]
+
+        for current in overlapping_periods:
+            previous = merged_periods[-1]
+            if current[0] <= previous[1]:
+                previous[1] = max(previous[1], current[1])
+            else:
+                merged_periods.append(current)
+
+        return merged_periods
+
+
+def plot_repeat_example_timeseries_figure(posts_df, posts_url_df, url_df):
+
+    account_name = 'Australian Climate Sceptics Group'
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.subplot()
+    
+    plt.title("'" + account_name + "' Facebook group")
+
+    account_id = posts_df[posts_df['account_name']==account_name].account_id.unique()[0]
+    posts_df_group = posts_df[posts_df["account_id"] == account_id]
+    plot_engagement_timeserie(ax, posts_df_group)
+    plt.ylim(-15, 150)
+
+    fake_news_dates = compute_fake_news_dates(posts_url_df, url_df, account_id)
+    for date in fake_news_dates:
+        plt.arrow(x=date, y=0, dx=0, dy=-15, color='C3')
+    plt.text(
+        s='Known strikes', color='C3', fontweight='bold',
+        x=np.datetime64('2019-09-15'), horizontalalignment='right', 
+        y=-3, verticalalignment='top'
+    )
+
+    repeat_offender_periods = compute_repeat_offender_periods(fake_news_dates)
+    repeat_offender_periods = merge_overlapping_periods(repeat_offender_periods)
+    for period in repeat_offender_periods:
+        plt.axvspan(period[0], period[1], ymin=1/11, facecolor='C3', alpha=0.1)
+    
+    patch1 = mpatches.Patch(facecolor='white', alpha=0.4, edgecolor='k')
+    patch2 = mpatches.Patch(facecolor='pink', alpha=0.4, edgecolor='k')
+    plt.legend([patch1, patch2], ["'No strike' periods", "'Repeat offender' periods"],
+               loc='upper left', framealpha=1)
+
+    plt.tight_layout()
+    save_figure('repeat_example_timeseries')
 
 
 def plot_repeat_average_timeseries(posts_df):
@@ -106,7 +213,12 @@ def plot_repeat_june_drop_percentage_change(posts_df):
 if __name__=="__main__":
 
     posts_df = import_crowdtangle_group_data()
-    # create_repeat_example_timeseries_figure(posts_df)
+
+    posts_url_df  = import_data(file_name="posts_url_2021-01-04_.csv")
+    posts_url_df = clean_crowdtangle_url_data(posts_url_df)
+    url_df = import_data(file_name="appearances_2021-01-04_.csv") 
+
+    plot_repeat_example_timeseries_figure(posts_df, posts_url_df, url_df)
 
     plot_repeat_average_timeseries(posts_df)
     plot_repeat_june_drop_percentage_change(posts_df)
